3d438d1e5cfcb7badcbfd1e5f6459e2beb995667
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-05 19:22:54 -0800

    loop w/rules completed

--- Program.cs
+++ Program.cs
@@ -7,8 +7,20 @@ namespace COMP003A.Assignment4
 
 class Program
 {
-    static void main()
+    static void main(string[] args)
     {
+        bool runProgram = true; //loop to keep program running until user exit
+        int stepCount = 0; //track # of completed steps
+        int maxSteps = 5; //max # of steps the routine will allow
+        int restrictedStep = 3; //step # to be skipped
+        int safetyLimit = 4; // safely stops the routine early 
+        //------------------------------------------------------
         
+
+
+
+
+
+
     }
 }
