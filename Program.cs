5fdf9a93b5f260b2649f3a0281fa5ca0c12584fd
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-05 20:13:10 -0800

    option 1 completed

--- Program.cs
+++ Program.cs
@@ -15,11 +15,33 @@ class Program
         int restrictedStep = 3; //step # to be skipped
         int safetyLimit = 4; // safely stops the routine early 
         //------------------------------------------------------
+        while (runProgram) //loop to repeat the menu and provide options to user
+        {
+            Console.WriteLine("\n1.Start/Continue routine");
+            Console.WriteLine("2. View Progress");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter option 1-3");
+
+            int option = int.Parse(Console.ReadLine()); //read user input
+
+            if (option == 1)
+            {
+                if (stepCount >= safetyLimit) //stops routine @ safety limit then prints the message and returns to menu
+                {
+                    Console.WriteLine("Safety limit reached, routine stopped");
+                    continue;
+                }
+
+                if (stepCount >= maxSteps) //stops the routine when max steps are reached and returns to menu
+                {
+                    Console.WriteLine("Routine Complete");
+                    continue;
+                }
+
+                stepCount++; //adv the routine 1 step
+
+                
         
-
-
-
-
 
 
     }
